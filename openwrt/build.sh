0c8475f794e34e68ebece4cebd5c37dcdd9a198f
jackdk86 <31080401+jackdk86@users.noreply.github.com>
2025-01-21 19:35:50 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -30,9 +30,9 @@ ip_info=`curl -sk https://ip.cooluc.com`;
 
 # script url
 if [ "$isCN" = "CN" ]; then
-    export mirror=https://init.cooluc.com
+    export mirror=https://raw.githubusercontent.com/jackdk86/r4s_build_script/refs/heads/master
 else
-    export mirror=https://init2.cooluc.com
+    export mirror=https://raw.githubusercontent.com/jackdk86/r4s_build_script/refs/heads/master
 fi
 
 # github actions - caddy server
